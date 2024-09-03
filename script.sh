ed1e75550220fab29cd66ed25c95b1e885d42d94
ahmetakif616 <77838245+ahmetakif616@users.noreply.github.com>
2024-09-03 18:18:26 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests/
+
+# repo init rom
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone https://github.com/ahmetakif616/local_manifests_clo -b RisingOS .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Sync
+/opt/crave/resync.sh
+echo "============="
+echo "Sync success"
+echo "============="
+
+# Export
+export BUILD_USERNAME=ahmetakif616
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+. build/envsetup.sh 
+echo "====== Envsetup Done ======="
+
+# Lunch
+riseup mi439 userdebug
+echo "============="
+
+# Build rom
+rise b
