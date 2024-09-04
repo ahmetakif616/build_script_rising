bf74b9dc191be45c7ee6f1d329ff208713293e75
ahmetakif616 <77838245+ahmetakif616@users.noreply.github.com>
2024-09-04 13:15:07 +0300

    Update script.sh
    Add 2 Cherry Pick from bliss to fix build error

--- script.sh
+++ script.sh
@@ -4,21 +4,31 @@ rm -rf .repo/local_manifests/
 
 # repo init rom
 repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
-echo "=================="
-echo "Repo init success"
-echo "=================="
+echo "======= Repo Init Done ======"
 
 # Local manifests
 git clone https://github.com/ahmetakif616/local_manifests_clo -b RisingOS .repo/local_manifests
-echo "============================"
-echo "Local manifest clone success"
-echo "============================"
+echo "======= Local Manifest Clone Done ======"
 
 # Sync
 /opt/crave/resync.sh
-echo "============="
-echo "Sync success"
-echo "============="
+echo "======= Sync Done ======"
+
+# cherry pick1
+cd hardware/interfaces
+git remote add bliss https://github.com/BlissRoms/platform_hardware_interfaces
+git fetch bliss
+git cherry-pick bbb2b5846645749fe3b778c1e50ac55378fd6235
+cd ../..
+
+# cherry pick2
+https://github.com/BlissRoms/platform_manifest/blob/universe/bliss.xml#L73C18-L73C32
+cd kernel/configs
+git remote add bliss1 https://github.com/BlissRoms/platform_kernel_configs
+git fetch bliss1
+git cherry-pick 5da1af4336121276bd65437e329c95c770923a35
+cd ../..
+echo "======= Cherry Picks Done ======"
 
 # Export
 export BUILD_USERNAME=ahmetakif616
@@ -31,7 +41,7 @@ echo "====== Envsetup Done ======="
 
 # Lunch
 riseup mi439 userdebug
-echo "============="
+echo "======= Build Starting ======"
 
 # Build rom
 rise b
